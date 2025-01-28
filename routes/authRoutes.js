6542b4564a3715eed289d448f8cb41acb5cf9727
AbdullahAlBitar <119155861+AbdullahAlBitar@users.noreply.github.com>
2025-01-28 19:26:58 +0300

    auth cleaned

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { validateRequest } = require('../middleware/validationMiddleware');
-const {signInSchema,signUpSchema, signUpSchemaEmployee} = require('./validationSchemas');
-const {authenticateJWT} = require('../middleware/authMiddleware');
+const { signInSchema, signUpSchema, signUpSchemaEmployee } = require('./validationSchemas');
+const { authenticateJWT } = require('../middleware/authMiddleware');
 const { isAdmin } = require('../middleware/authRole')
 const encry = require('../middleware/encryptionMiddleware')
 const encryM = require('../middleware/mobileEncryptionMiddleware')
@@ -11,7 +11,7 @@ const encryR = require('../middleware/regMobileEncryptionMiddleware')
 
 
 router.post('/login', encryM.decryptionMobile, validateRequest(signInSchema), authController.login);
-router.post('/register',encryR.decryptionMobile, validateRequest(signUpSchema), authController.register);
+router.post('/register', encryR.decryptionMobile, validateRequest(signUpSchema), authController.register);
 
 router.post('/dashboard/login', encry.decryption, validateRequest(signInSchema), authController.loginDashboard);
 router.post('/register/employee', authenticateJWT, isAdmin, encry.decryption, validateRequest(signUpSchemaEmployee), authController.registerEmployee);
--- services/authService.js
+++ services/authService.js
@@ -1,12 +1,26 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+async function emailLogin(email, password) {
+  const user = await prisma.users.findUnique({
+    where:{
+      email
+    }
+  });
 
-
-async function disconnect() {
-  await prisma.$disconnect();
+  if (!user) {
+    let error = new Error("Not Found");
+    error.meta = { code: "404", error: 'User not found' };
+    throw error;
+  } else if (await bcrypt.compare(password, user.password)) {
+    return user;
+  } else {
+    let error = new Error("Wrong password");
+    error.meta = { code: "409", error: 'Password is wrong' };
+    throw error;
+  }
 }
 
 module.exports = {
-  disconnect,
+  emailLogin,
 };
